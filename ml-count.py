3cb209a280f01ca85f495bba30b126ecf24a0284
Kevin Liu <rla185@sfu.ca>
2024-11-28 16:38:45 -0800

    add ml recommendation model code

--- ml-count.py
+++ ml-count.py
@@ -1,11 +1,15 @@
 import sys
 
+from pyspark.ml import Pipeline
+
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 from pyspark.sql import SparkSession, functions as sf, types, Row
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
+from pyspark.ml.feature import StringIndexer
 
-PATH = '/Users/kevin/PycharmProjects/finalGit/New'
+PATH = './New'
+
 
 def main():
     # read rdd
@@ -86,6 +90,45 @@ def main():
 
     # show data
     # final_data.show()
+
+    # ALS Recommendation Model
+    # find the number of purchase of items of each user
+    data = final_data.groupBy(final_data.user_id, final_data.product_id).count()
+
+    # use a string indexer to transform the (long) id to (int) id
+    data = data.select(data['user_id'].cast('string'), data['product_id'].cast('string'), data['count'])
+    indexer1 = StringIndexer(inputCol="user_id", outputCol="user_id_index")
+    indexer2 = StringIndexer(inputCol="product_id", outputCol="product_id_index")
+    model1 =  indexer1.fit(data)
+    temp = model1.transform(data)
+    model2 = indexer2.fit(temp)
+    ready_to_use = model2.transform(temp)
+    ready_to_use = (ready_to_use.withColumn('user_id', ready_to_use['user_id_index'].cast('int'))
+                    .withColumn('product_id', ready_to_use['product_id_index'].cast('int')))
+    ready_to_use.drop('user_id_index').drop('product_id_index')
+
+    # split data
+    (training, test) = ready_to_use.randomSplit([0.8, 0.2])
+    training.cache()
+    test.cache()
+
+    # build and train model
+    als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="product_id", ratingCol="count",
+              coldStartStrategy="drop", implicitPrefs=True)
+    model = als.fit(training)
+
+    # Evaluate the model by computing the RMSE on the test data
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",
+                                    predictionCol="prediction")
+    rmse = evaluator.evaluate(predictions)
+    print("Root-mean-square error = " + str(rmse))
+
+    # Generate top 2 product recommendations for each user
+    model.recommendForAllUsers(2).show(10)
+
+
+
 
 
 
